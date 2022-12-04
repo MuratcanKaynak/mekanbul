b0e7038e0584c9615fd58ce04911f6559defbc8b
MuratcanKaynak <muratcan.kaynak39@gmail.com>
2022-12-04 17:08:48 +0300

    pahali

--- app_api/models/db.js
+++ app_api/models/db.js
@@ -1,43 +1,25 @@
-var mongoose=require("mongoose");
+var mongoose = require("mongoose");
 require("./mekansema");
-<<<<<<< HEAD
-var dbURI="mongodb://localhost/mekanbul";
-//var dbURI = 'mongodb+srv://muratcankaynak:webodevi123@mekanbul.q6qlga7.mongodb.net/mekanbul?retryWrites=true&w=majority'; 
-mongoose.connect(dbURI); 
-function kapat(msg,callback){
-    mongoose.connection.close(function(){
-        console.log(msg);
-        callback();
-    }
-    );
-=======
 var dbURI = "mongodb://localhost/mekanbul";
-//var dbURI="mongodb+srv://muratcankaynak:<webodevi123>@mekanbul.q6qlga7.mongodb.net/?retryWrites=true&w=majority";
+//var dbURI="mongodb+srv://yagizsahinler:yagiz123@mekanbul.63scdws.mongodb.net/mekanbul?retryWrites=true&w=majority";
 mongoose.connect(dbURI);
 function kapat(msg, callback) {
   mongoose.connection.close(function () {
     console.log(msg);
     callback();
   });
->>>>>>> 46977a7 (kesin1)
 }
-process.on("SIGINT",function(){
-kapat("Uygulama kapatıldı!",function(){
+process.on("SIGINT", function () {
+  kapat("Uygulama kapatıldı!", function () {
     process.exit(0);
+  });
 });
+mongoose.connection.on("connected", function () {
+  console.log(dbURI + "adresindeki veritabanına bağlandı");
 });
-mongoose.connection.on("connected",function(){
-console.log(dbURI+"adresindeki veritabanına bağlandı");
-}
-
-);
-mongoose.connection.on("disconnected",function(){
-    console.log(dbURI+"adresindeki veritabanı bağlantısı koptu");
-}
-
-);
-mongoose.connection.on("error",function(){
-    console.log("bağlantı hatası");
-}
-
-);
+mongoose.connection.on("disconnected", function () {
+  console.log(dbURI + "adresindeki veritabanı bağlantısı koptu");
+});
+mongoose.connection.on("error", function () {
+  console.log("bağlantı hatası");
+});
--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -1,116 +1,44 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-<<<<<<< HEAD
-
-
-const anaSayfa = function (req, res, next) {
-  res.render('anasayfa',
-  {
-    "baslik": 'Anasayfa' ,
-    "sayfaBaslik":{
-      "siteAd":"Mekanbul",
-      "slogan":"Civardaki Mekanları Keşfet! "
-  },
-  "mekanlar":[
-    {
-      "ad":"Starbucks",
-      "adres":"Centrum Garden Avm",
-      "puan":"3",
-      "imkanlar":["Dünya Kahveleri","Yiyecek","Hızlı wifi bağlantısı"],
-      "mesafe":"10km"
-    },
-    {
-      "ad":"Barida Kafe",
-      "adres":"Sdü Batı Kampüsü",
-      "puan":"4",
-      "imkanlar":["Dünya Kahveleri","Yiyecek","Hızlı wifi bağlantısı"],
-      "mesafe":"1km"
-    }
-  ]
-  });
-}
-
-const mekanBilgisi = function (req, res, next) {
-  res.render('mekanbilgisi',
-  { 
-  "baslik": 'Mekan Bilgisi',
-  "mekanBaslik":"Starbucks",
-  "mekanDetay":{
-    "ad":"Starbucks",
-    "puan":"3",
-    "adres":"Centrum Garden",
-    "saatler":[
-      {
-        "gunler":"Pazartesi-Cuma",
-        "acilis":"9:00",
-        "kapanis":"23:00",
-        "kapali":false
-      },
-      {
-        "gunler":"Cumartesi-Pazar",
-        "acilis":"10:00",
-        "kapanis":"22:00",
-        "kapali":false
-      }
-    ],
-    "imkanlar":["Dünya Kahveleri","Yiyecek","Hızlı wifi bağlantısı"],
-    "koordinatlar":{
-      "enlem":"37.7",
-      "boylam":"30.5"
-    },
-    "yorumlar":[
-      {
-        "yorumYapan":"Asım Sinan Yüksel",
-        "yorumMetni":"Berbaaaaaat",
-        "tarih":"20 Ekim 2022",
-        "puan":"1"
-      },
-      {
-        "yorumYapan":"Ali",
-        "yorumMetni":"Süper",
-        "tarih":"20 Ekim 2022",
-        "puan":"5"
-      }
-    ]
-=======
 const axios=require("axios");
-const e = require("express");
 var apiSecenekleri = {
-  sunucu: "localhost:3000",
+  sunucu: "http://localhost:3000/",
   apiYolu: "/api/mekanlar/",
-}
+};
 var mesafeyiFormatla=function(mesafe){
   var yeniMesafe,birim;
   if(mesafe>1){
     yeniMesafe=parseFloat(mesafe).toFixed(1);
-    birim=" km";
-  }else{
-    yeniMesafe=parseInt(mesafe*1000,10);
-    birim=" m";
+    birim= " km";
+  } else {
+  yeniMesafe=parseInt(mesafe*1000,10);
+  birim=" m";
   }
   return yeniMesafe+birim;
 }
+
 var anaSayfaOlustur=function(res,mekanListesi){
   var mesaj;
-  if(!(mekanListesi instanceof(Array))){
-  mesaj="API HATASI:Birşeyler ters gitti.";
-  mekanListesi=[];
+  if(!(mekanListesi instanceof Array)){
+    mesaj="API HATASI:Bir şeyler ters gitti.";
+    mekanListesi=[];
   }else{
-  if(!mekanListesi.length){
-  mesaj="Civarda herhangi bir mekan yok";
-  }
+    if(!mekanListesi.length){
+      mesaj="Civarda herhangi bir mekan yok";
+    }
   }
   res.render("anasayfa",{
     "baslik":"Anasayfa",
-    "sayfaBaslik":{
+    sayfaBaslik:{
       "siteAd":"Mekanbul",
       "slogan":"Mekanları Keşfet"
     },
-    "mekanlar":mekanListesi,
-    "mesaj":mesaj
+    mekanlar:mekanListesi,
+    mesaj:mesaj
   });
 }
+
 const anaSayfa = function (req, res, next) {
   axios.get(apiSecenekleri.sunucu+apiSecenekleri.apiYolu,{
     params:{
@@ -129,29 +57,29 @@ const anaSayfa = function (req, res, next) {
   });
 }
 
-var detaySayfasiOlustur = function(res,mekanDetaylari){
+var detaySayfasiOlustur = function (res,mekanDetaylari) {
   mekanDetaylari.koordinat={
-    "enlem":mekanDetaylari.koordinat[0],
-    "boylam":mekanDetaylari.koordinat[1]
+    "enlem" : mekanDetaylari.koordinat[0],
+    "boylam" : mekanDetaylari.koordinat[1]
   }
   res.render('mekanbilgisi',
   {
-    mekanBaslik: mekanDetaylari.ad,
-    mekanDetay:mekanDetaylari
-  })
+    mekanBaslik : mekanDetaylari.ad,
+    mekanDetay: mekanDetaylari
+  });
 }
 
-var hataGoster = function(res,hata){
+var hataGoster = function(res, hata) {
   var mesaj;
-  if(hata.response.status==404){
-    mesaj="404, Sayfa Bulunamadı!";
+  if(hata.response.status==404) {
+    mesaj = "404, Sayfa Bulunamadı!";
   }
-  else{
+  else {
     mesaj=hata.response.status+" hatası";
   }
   res.status(hata.response.status);
   res.render('error',{
-    "mesaj":mesaj
+    "mesaj": mesaj
   });
 };
 
@@ -161,21 +89,17 @@ const mekanBilgisi = function (req, res) {
     .then(function (response){
       detaySayfasiOlustur(res, response.data);
     })
-    .catch(function (hata) {
+    .catch(function(hata){
       hataGoster(res, hata);
     });
 };
->>>>>>> 46977a7 (kesin1)
 
-  }
-});
-};
 const yorumEkle = function (req, res, next) {
-  res.render('yorumekle', { title: 'Yorum Ekle' });
+  res.render("yorumekle", { title: "Yorum Ekle" });
 };
+
 module.exports = {
   anaSayfa,
   mekanBilgisi,
-  yorumEkle
-
-}
+  yorumEkle,
+};
